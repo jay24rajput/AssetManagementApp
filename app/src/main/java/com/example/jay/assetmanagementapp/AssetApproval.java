f395d95d79a885779ff5f6f3c8c1bd39512fad32
Jay Rajput <jay24rajput@gmail.com>
2018-09-24 10:27:50 +0530

    Added back button to the remaining pages

--- app/src/main/java/com/example/jay/assetmanagementapp/AssetApproval.java
+++ app/src/main/java/com/example/jay/assetmanagementapp/AssetApproval.java
@@ -3,6 +3,7 @@ package com.example.jay.assetmanagementapp;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class AssetApproval extends AppCompatActivity {
 
@@ -11,5 +12,21 @@ public class AssetApproval extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         setContentView(R.layout.activity_asset_approval);
         super.onCreate(savedInstanceState);
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                //Write your logic here
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
